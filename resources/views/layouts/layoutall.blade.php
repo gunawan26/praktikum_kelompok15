a7c4f15d55d6607d4ea4bf8cdc23fd83cbe73f69
I Putu Iduar Perdana <37173696+iduarperdana@users.noreply.github.com>
2018-04-21 14:06:12 +0800

    search

--- resources/views/layouts/layoutall.blade.php
+++ resources/views/layouts/layoutall.blade.php
@@ -27,12 +27,7 @@
           <a class="navbar-brand" href="index.html"><img src="{{asset('img/logo.png')}}" class="img-fluid" alt="logo"></a> </ul>
 
           <ul class=" navbar-nav col-md-7">
-            <div class="input-group">
-              <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-success" type="button">Search</button>
-              </div>
-            </div>
+            
           </ul>
           <ul class="navbar-nav col-md-1"></ul>
           <ul class="navbar-nav col-md-2">
@@ -51,11 +46,28 @@
 
 
    <header class="bg-gradient" id="home">
-    <div class="container">
-      <hr class="featurette-divider">
-    </div>
-    
-  </header>
+                <div class="container mt-4" style="width: 70%; height: 70px;background-color: #6E6E6E; padding-top: 10px; padding-bottom: 10px;" >
+                  <div class="input-group" style="height: 50px;">
+                   <input type="text" class="form-control" placeholder="Semua merk mobil" aria-label="" aria-describedby="basic-addon1">
+                   <select class="custom-select" id="inputGroupSelect04" style="height: 50px;">
+                    <option selected>Sewa</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </select>
+                  <div class="input-group-append" >
+                    <button class="btn btn-success" type="button">Cari</button>
+                  </div>
+                </div>
+              </div>
+
+              <div class="container mt-4">
+                <h1 class="text-secondary">Website Rentcar Terbaik Di indonesia </h1>
+                <p class="tagline text-secondary">Sewa aman, jalan aman</p>
+                <hr class="featurette-divider">
+              </div>
+
+            </header>
   
 
 
