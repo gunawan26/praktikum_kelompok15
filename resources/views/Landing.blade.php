26fc0b9aab94efac4105d91fe957cabd5e81728f
I Putu Iduar Perdana <37173696+iduarperdana@users.noreply.github.com>
2018-05-23 08:39:01 +0800

    landing
    landing

---
+++ resources/views/Landing.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.landing')
+
+@section('content')
+<!-- Icons Grid -->
+    <section class="features-icons bg-light text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                <i class="icon-screen-desktop m-auto text-primary"></i>
+              </div>
+              <h3>Pelayanan</h3>
+              <p class="lead mb-0">Pelayanan terbaik untuk pemilik </p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                <i class="icon-layers m-auto text-primary"></i>
+              </div>
+              <h3>Aman</h3>
+              <p class="lead mb-0">Aman dalam transaksi rentcar</p>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="features-icons-item mx-auto mb-0 mb-lg-3">
+              <div class="features-icons-icon d-flex">
+                <i class="icon-check m-auto text-primary"></i>
+              </div>
+              <h3>Mudah</h3>
+              <p class="lead mb-0">Mudah dalam transaksi antara pemilik dengan penyewa</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Image Showcases -->
+    <section class="showcase">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/car1.jpg');"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+            <h2>Pelayanan Transaksi</h2>
+            <p class="lead mb-0">Pelayanan terjamin paling terbaik</p>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/car2.jpg');"></div>
+          <div class="col-lg-6 my-auto showcase-text">
+            <h2>Aman</h2>
+            <p class="lead mb-0">Keamanan dalam transaksi adalah salah satu dari keuntungan bergabung dengan website rentcar.com</p>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/car1.jpg');"></div>
+          <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+            <h2>Mudah</h2>
+            <p class="lead mb-0">Kemudahan dalam transaksi menjadi hal yang diinginkan oleh pemilik maupun penyewa </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+@endsection
---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>Rentcar</title>
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('css/simple-line-icons.css" rel="stylesheet')}}" type="text/css">
+  <link href="{{ asset('css/landing-page.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+  
+</head>
+
+<body data-spy="scroll" data-target="#navbar" data-offset="30">
+  <!-- Nav Menu -->
+
+  <div class="nav-menu fixed-top">
+    <div class="container">
+
+      <nav class="navbar navbar-dark navbar-expand-lg">
+        <ul class=" navbar-nav mr-auto">
+          <a class="navbar-brand" href="index.html">
+            <img src="{{asset('img/logo.png')}}" class="img-fluid" alt="logo">
+          </a>        
+          <li class="nav-item ">
+            <a class="nav-link text-secondary" href="#">HOME
+              
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link text-secondary" href="#">PARTNER</a>
+            <span class="sr-only">(current)</span>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link text-secondary" href="#">ABOUT</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          @guest
+            <li class="dropdown">
+          <button type="button" class="btn btn-outline-secondary dropdown"   id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Daftar</button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <div class="card">
+  <div class="card-body">
+    <div class="btn-group " role="group" aria-label="Basic example">
+            <a href="{{ url('/pemilik/register') }}" class="btn btn-primary "> Pemilik</a>
+            <a href="{{ url('/register') }}" class="btn btn-success "> Penyewa</a>
+          </div></div></div>
+  </div>
+          </li>
+          
+          <li class="dropdown ml-lg-3">
+          <button type="button" class="btn btn-outline-success dropdown"   id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Masuk</button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <div class="card">
+  <div class="card-body">
+           <div class="btn-group " role="group" aria-label="Basic example">
+            <a href="{{ url('/pemilik/login') }}" class="btn btn-primary "> Pemilik</a>
+            <a href="{{ url('/login') }}" class="btn btn-success "> Penyewa</a>
+          </div></div></div>
+  </div>
+          </li>
+
+          @else
+
+          <li class="nav-item">
+            <div class="nav-link text-secondary">
+              Welcome {{Auth::user()->nama_depan}}
+            </div>
+
+          </li>
+          <li class="nav-item">
+            <form action="{{route('logout')}}" method="post" >
+              @csrf
+
+
+              <button type="submit" class="btn btn-outline-success my-3 my-sm-0">Logout</button>
+
+            </form>
+
+          </li>
+
+          @endguest
+
+
+        </ul>
+
+      </nav>
+
+    </div>
+  </div>
+  <!-- End Nav Menu -->
+
+
+  <!-- Masthead -->
+    <header class="masthead text-white text-center">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <h1 class="mb-5">Ayo Bergabung untuk mengembangkan usaha rentcar anda menjadi rentcar yang modern</h1>
+          </div>
+          <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <div class="col-12 ">
+                  <button type="submit" class="btn btn-block btn-lg btn-success">Daftar</button>
+                </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+
+
+
+  <main>
+    @yield('content')
+  </main>
+
+  <div class="client-logos mt-5">
+    <div class="container text-center">
+      <img src="{{asset('img/honda.png')}}" width="100px" class="img-fluid" alt="logo">
+      <img src="{{asset('img/toyota.png')}}" width="100px" class="img-fluid" alt="logo">
+      <img src="{{asset('img/suzuki.png')}}" width="100px" class="img-fluid" alt="logo">
+      <img src="{{asset('img/daihatsu.png')}}" width="100px" class="img-fluid" alt="logo">
+    </div>
+  </div>
+  <footer class="my-5 text-center">
+    <!-- Copyright removal is not prohibited! -->
+    <p class="mb-2">
+      <small>COPYRIGHT © 2018. ALL RIGHTS RESERVED. MOBAPP TEMPLATE BY RENTCAR</a>
+      </small>
+    </p>
+
+    <small>
+      <a href="#" class="m-2">PRESS</a>
+      <a href="#" class="m-2">TERMS</a>
+      <a href="#" class="m-2">PRIVACY</a>
+    </small>
+  </footer>
+
+
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('js/script.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
+  @yield('custom-js')
+
+</body>
+
+</html>
